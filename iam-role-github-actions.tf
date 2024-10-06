bb6f7fede0386b58020b906bd731b7aa3e05b900
Hellsingi <il.dzemchanka@gmail.com>
2024-10-06 14:59:03 +0200

    Task 1 create basic configuration for create role and s3 use terraform

---
+++ iam-role-github-actions.tf
@@ -0,0 +1,46 @@
+# Create the IAM OIDC provider for GitHub Actions
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]  # GitHub OIDC thumbprint
+}
+
+# Create the IAM role for GitHub Actions
+resource "aws_iam_role" "GithubActionsRole" {
+  name = "GithubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub": "repo:Hellsingi/rsschool-devops-course-tasks:ref:refs/heads/task_1"
+          }
+        }
+      }
+    ]
+  })
+
+  # Attach the necessary AWS managed policies for full access to EC2, S3, Route53, etc.
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/IAMFullAccess",
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
+    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+  ]
+}
+
+# Output the IAM role ARN for GitHub Actions
+output "github_actions_role_arn" {
+  value = aws_iam_role.GithubActionsRole.arn
+}
