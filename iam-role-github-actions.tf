9461d8239991ee313659226b3f91157f3d4d4934
Hellsingi <il.dzemchanka@gmail.com>
2024-10-13 16:30:15 +0200

    Test 1

--- iam-role-github-actions.tf
+++ iam-role-github-actions.tf
@@ -1,46 +1,32 @@
-# Create the IAM OIDC provider for GitHub Actions
-resource "aws_iam_openid_connect_provider" "github" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
-}
-
-# Create the IAM role for GitHub Actions
 resource "aws_iam_role" "GithubActionsRole" {
   name = "GithubActionsRole"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+    "Version" : "2012-10-17",
+    "Statement" : [
       {
-        Effect = "Allow",
-        Principal = {
-          Federated = aws_iam_openid_connect_provider.github.arn
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::442042521792:oidc-provider/token.actions.githubusercontent.com"
         },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringEquals = {
-            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" : "repo:Hellsingi/rsschool-devops-course-tasks:ref:refs/heads/task_1"
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : "repo:liubov-js/rsschool-devops-course-tasks:*"
           }
         }
-      },
-      {
-        Effect = "Allow",
-        Principal = {
-          AWS = "arn:aws:iam::588738612192:user/UserTask1"
-        },
-        Action = "sts:AssumeRole"
       }
     ]
   })
 
   tags = {
-    Name = "GitHub Actions Role"
+    tag-key = "GitHub Actions Role"
   }
 }
 
-# Attach individual policies to the IAM role
 resource "aws_iam_role_policy_attachment" "ec2_full_access" {
   role       = aws_iam_role.GithubActionsRole.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
@@ -74,9 +60,4 @@ resource "aws_iam_role_policy_attachment" "sqs_full_access" {
 resource "aws_iam_role_policy_attachment" "eventbridge_full_access" {
   role       = aws_iam_role.GithubActionsRole.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
-}
-
-# Output the IAM role ARN for GitHub Actions
-output "github_actions_role_arn" {
-  value = aws_iam_role.GithubActionsRole.arn
 }
